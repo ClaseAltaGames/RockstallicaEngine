26f3d1eed224a1507dd2b3fb124b166fc5c79956
Edgar Mesa <118179227+edgarmd1@users.noreply.github.com>
2024-12-17 17:03:32 +0100

    ahora la camara es un componente con configuraciones que se pueden modificar
    Co-Authored-By: Pau Mena Torres <117993994+PauMenaTorres@users.noreply.github.com>

---
+++ Engine/Engine/ComponentCamera.h
@@ -0,0 +1,31 @@
+// ComponentCamera.h
+#pragma once
+#include "Component.h"
+#include "glm/glm.hpp"
+
+class ComponentCamera : public Component
+{
+public:
+    ComponentCamera(GameObject* owner);
+    ~ComponentCamera() override;
+
+    void Update(float dt);
+    void OnEditor() override; // Mostrar opciones en el Inspector
+    void LookAt(const glm::vec3& target);
+
+    void SetFOV(float fov);
+    void SetNearPlane(float nearPlane);
+    void SetFarPlane(float farPlane);
+
+    glm::mat4 GetViewMatrix() const;
+    glm::mat4 GetProjectionMatrix() const;
+
+public:
+    float fov = 60.0f;
+    float nearPlane = 0.1f;
+    float farPlane = 1000.0f;
+
+private:
+    glm::vec3 position;
+    glm::vec3 reference;
+};
